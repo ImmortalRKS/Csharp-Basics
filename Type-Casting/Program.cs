cfe2efcb92e0340f9bfa38795e9bca3167ce06b7
ImmortalRKS <rishitks1307@gmail.com>
2026-06-17 21:25:38 +0530

    Changing data type of var

---
+++ Type-Casting/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 30;
+double b = Convert.ToDouble(a);
+Console.WriteLine(b); //will only show 30 not decimal
+Console.WriteLine(b + 0.2); //then it will add 0.2
+string c = Convert.ToString(a);
+Console.WriteLine(c);
+Console.WriteLine(c.GetType()+c);
+
+string d = "Hello";
+char e = Convert.ToChar(d);
+Console.WriteLine(e); //in here it as you know that char can only store one value in it, so in this case, it wont show error in vscode
+                      //but when you run it, the code will run till this code and then it will show "unhandled exception, string must be one command long"
+
+string f = "fahh";
+bool g = Convert.ToBoolean(f);
+Console.WriteLine("g");  //it also shows same error like char, saying fahh was not a valid boolean, ofc the code wont run till here, unless u remove upper garbage
+
+
+
