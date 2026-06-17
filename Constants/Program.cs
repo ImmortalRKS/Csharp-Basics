6e0035abe80fe87991e5bffad807298061d1db56
ImmortalRKS <rishitks1307@gmail.com>
2026-06-17 20:24:49 +0530

    Use of Constant

---
+++ Constants/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 10;
+
+a = 40;
+
+Console.WriteLine("a is : "+ a);  //as you can see that value of a changes from the initial value given
+
+const int b = 20;
+
+Console.WriteLine("b is : "+ b); //value of b cannot be added and even if you try to do it, it will show error
+
+
