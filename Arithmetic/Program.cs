e3ade3c40e623217a74e49a9f69f2788cc215390
Immortal <rishitks1307@gmail.com>
2026-06-18 18:05:31 +0530

    Arithmetic Operaters

---
+++ Arithmetic/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 10; // this is literally python wth am i doing it lol
+a +=20;
+Console.WriteLine(a);
+
+int b = 12;
+Console.WriteLine(b);
+b--;
+--b;  //god knows what is pre decrement/incremnet and what is post increment/decrement
+Console.WriteLine(b);
+
+
+int c = 10;
+c += 2;
+Console.WriteLine(c);
+c = c/2;
+Console.WriteLine(c);
+
+//okay enough of this lol, need to go to better things
