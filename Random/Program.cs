7f99b54574daeb737ce2c5bd95cbf83219c69c32
Immortal <rishitks1307@gmail.com>
2026-06-19 21:00:13 +0530

    Random

---
+++ Random/Program.cs
@@ -0,0 +1,5 @@
+﻿Random a = new Random();
+int b = a.Next(1,7); //here we are making a basic dice rolling function
+Console.WriteLine("We are rolling a dice and the output is : " + b);
+
+//the first random is the data type and new random() makes a random number generater which generated pseudo random numbers
