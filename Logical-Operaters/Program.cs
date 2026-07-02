7d88dbaf09ba31e60067f5f0cc7bacd7483669df
Immortal <rishitks1307@gmail.com>
2026-07-02 09:43:35 +0530

    Logical Operater

---
+++ Logical-Operaters/Program.cs
@@ -0,0 +1,15 @@
+﻿//Loigcal Operaters
+// && (and) , || (or), its basically and, or from python which is used in true and false
+
+Console.WriteLine("What temperature is outside? (C) : ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+if (a >= 10 && a <= 25)
+{
+    Console.WriteLine("Its warm outside");
+}
+
+else if (a <= -50 || a >= 50)
+{
+    Console.WriteLine("Dont go outside man, you gonna die ahhh");
+}
