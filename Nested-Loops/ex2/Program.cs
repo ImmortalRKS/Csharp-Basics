21831d2ea27dde5a3f81111d77c8cdf310a39d86
Immortal <rishitks1307@gmail.com>
2026-07-19 06:08:59 +0000

    star pyramiddd left faced

--- Nested-Loops/ex2/Program.cs
+++ Nested-Loops/ex2/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//trying to make pyramid pattern on my own
+//god knows will it even or not fahh
+
+
+Console.Write(" Enter Height : ");
+int h = Convert.ToInt32(Console.ReadLine());
+
+for (int a = 1; a <= h; a++)
+{
+    for ( int b = 1; b <= a; b++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
