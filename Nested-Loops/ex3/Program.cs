6fb8a253ea18805fd13aa60de66e49541a6bb750
Immortal <rishitks1307@gmail.com>
2026-07-19 06:12:17 +0000

    God Knows

---
+++ Nested-Loops/ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write(" Enter Height : ");
+int h = Convert.ToInt32(Console.ReadLine());
+
+for (int a = 1; a <= h; a++)
+{
+    for ( int b = 1; b <= a; b++)
+    {
+        for ( int c = 1; c <= b; c++)
+        {
+            Console.Write("*");
+        }
+    }
+    Console.WriteLine();
+}
+// god knows yeh kya baaan gaya
