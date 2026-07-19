8004a9d52185d12f34211419398b307443e84cea
Immortal <rishitks1307@gmail.com>
2026-07-19 06:14:39 +0000

    OG Pyramid

---
+++ Nested-Loops/ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter height: ");
+int height = Convert.ToInt32(Console.ReadLine());
+
+for (int row = 1; row <= height; row++)
+{
+    // Print spaces
+    for (int space = 1; space <= height - row; space++)
+    {
+        Console.Write(" ");
+    }
+
+    // Print stars
+    for (int star = 1; star <= row * 2 - 1; star++)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();
+}
+
+//although i havent wrote this, its from chatgpt, i was still able to understand it better
