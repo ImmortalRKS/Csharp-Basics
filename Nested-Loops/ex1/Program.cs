ccefb94f60502605ceaaa5c8dde2803bf7cade10
Immortal <rishitks1307@gmail.com>
2026-07-19 05:31:02 +0000

    1st ver

---
+++ Nested-Loops/ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+Console.Write("Enter Total Number Of Rows : ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Total Number Of Coloumns : ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter which symbol to use : ");
+char symbol = Convert.ToChar(Console.ReadLine())!;
+
+
+for (int i = 0; i<rows ; i++)
+{
+    for (int j = 0; j<columns ;j++)
+    {
+        Console.Write(symbol);
+    }
+    Console.WriteLine();
+}
